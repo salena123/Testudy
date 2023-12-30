dbfe6adce14f68828d542e4d44056eb57a365746
алена <sasmurka@mail.ru>
2023-12-30 05:36:35 +0500

    тест

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const radioButtons = document.querySelectorAll('.radio-button');
+const customButtons = document.querySelectorAll('.custom-radio');
+
+const getChecked = (radioButton) => {
+    radioButton.addEventListener ('change', () => {
+        if(radioButton.checked) {
+            const currentButton = radioButton.parentElement.querySelector('img');
+            currentButton.src = '/img/check_ring.svg';
+            for (let radioButton of radioButtons) {
+                if (!radioButton.checked) {
+                    radioButton.parentElement.querySelector('img').src = '/img/no-check_ring.svg';
+                }
+            }
+        }
+    });
+
+    
+};
+
+for (let radioButton of radioButtons) {
+    getChecked(radioButton);
+}
+
